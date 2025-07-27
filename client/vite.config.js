ccaa6a6d03f25008542b7c1db80e912fd54b5c8a
Sohil Khalifa <157364373+Sohilkhalifa432@users.noreply.github.com>
2025-07-27 19:38:27 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  // server proxy development के लिए है, production में काम नहीं आएगा
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+      '/api': {
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
