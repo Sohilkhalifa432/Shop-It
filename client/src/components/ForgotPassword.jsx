ea1a50037600ab66b9c0d63d1a96f2ace415d0f9
Sohil Khalifa <157364373+Sohilkhalifa432@users.noreply.github.com>
2025-07-28 15:09:02 +0530

    Update ForgotPassword.jsx

--- client/src/components/ForgotPassword.jsx
+++ client/src/components/ForgotPassword.jsx
@@ -10,7 +10,7 @@ const ForgotPassword = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/forgot-password", {
+      const res = await axios.post("https://shop-it-1-9q81.onrender.com/api/v1/auth/forgot-password", {
         email,
         newPassword,
         answer,
